0d6024f419c18c8b519a6f599abf145a06d4e351
PanitanK <98577467+PanitanK@users.noreply.github.com>
2023-11-17 00:27:53 +0700

    1227

--- carbond/src/Routes/Home.js
+++ carbond/src/Routes/Home.js
@@ -23,36 +23,18 @@ function Home() {
   const navigate = useNavigate();
   const [userData, setUserData] = useState(null);
   const [HasFetched, setFetch] = useState(false);
-  //const [showSetting, setShowSetting] = useState(false); // State to track whether to show Setting component
-  const [plotDocuments, setplotDocuments] = useState([])
-  //const [showDropdown, setShowDropdown] = useState(false);
 
-  const [MaximumCredit , setMaximumCredit] = useState(0);
 
-  console.log(plotDocuments)
-  console.log(MaximumCredit)
+
+
+  
   const handleNavigation = (path) => {
-    // Use the navigate function to navigate to the specified path
     navigate(path);
   };
   
   
  
-  const fetchPlotDocuments = async (userUID) => {
-    const dataCollectionRef = collection(db, 'USERS', userUID, 'DataCollection');
-  
-    try {
-      const querySnapshot = await getDocs(dataCollectionRef);
-      const plotDocuments = querySnapshot.docs
-        .filter((doc) => doc.id.startsWith('PlotNO_'))
-        .map((doc) => doc.data());
-        
-      return plotDocuments;
-    } catch (error) {
-      console.error('Error fetching plot documents:', error);
-      return [];
-    }
-  };
+
   
 
   useEffect(() => {
@@ -60,20 +42,9 @@ function Home() {
       try {
         const userDocRef = doc(userCollectionRef, userUID);
         const userDocSnapshot = await getDoc(userDocRef);
-        const plotDocuments = await fetchPlotDocuments(userUID);
-    
-        setplotDocuments(plotDocuments)
-
-        var sum = 0;
-        for (let i = 0; i < plotDocuments.length; i++) {
-          sum = sum + ((44/12)*((plotDocuments[i].Area / 0.0016)*230*0.65 * 0.8825  / (1000)))
-        }
-
-  setMaximumCredit(sum)
         if (userDocSnapshot.exists()) {
           const USERS_UID_SubCollection = collection(userDocRef, 'ProfileCollection');
           const USERS_UID_SubCollection_Snapshot = await getDocs(USERS_UID_SubCollection);
-
           const userData = USERS_UID_SubCollection_Snapshot.docs.map((doc) => doc.data());
           setUserData(userData);
         } else {
@@ -108,6 +79,7 @@ function Home() {
   
   else {
     return (
+      /* STATIC BAR */
       <div className="App">
         <div className="static-bar">
         <div className="left-content">
@@ -115,14 +87,15 @@ function Home() {
             <img className="title-image" src={Title} alt="Title" />
           </Link>
         </div>
-        <Link to="/login" className="green-box">
-          <img src={UserIcon} alt="UGBN" className="Locker" />
-          <h1 className="green-box-text">Wei Shen</h1>
-        </Link>
-
+        <div className="green-box2">
+          <img src={UserIcon} alt="UGBN" className="UserIcon" />
+          <h1 className="green-box-text2"> {userData[0].Name}</h1>
+        </div>
       </div>
+
+
+
       <div className='Dashboard'>
-        
       <div className="sidebar">
         <div className="sidebar-option" onClick={() => handleNavigation('/option1')}>
           <img src={HomeIcon} alt="UGBN" className="IconClass" />
@@ -148,8 +121,7 @@ function Home() {
       
     </div>
     <div className='ContentBoard'>
-      <h1>555</h1>
-      <h1>555</h1>
+    
 
     </div>
     
