59431fe4c49147118c31145c6e7601d3f1b44854
PanitanK <98577467+PanitanK@users.noreply.github.com>
2023-11-18 10:53:08 +0700

    1053

--- carbond/src/Routes/Req.js
+++ carbond/src/Routes/Req.js
@@ -17,30 +17,44 @@ const Request = () => {
   };
 
   useEffect(()=> {
-    console.log("Use effect JSON")
-    console.log(jsonData);
+    /*console.log("Use effect JSON")
+    console.log("JSON0");
+    console.log(jsonData.PlotData.Area)
+    console.log(jsonData.PlotData.PlotCenter[0])
+    console.log(jsonData.PlotData.PlotCenter[1])
+    console.log(jsonData)*/
   })
  
 
   return (
     <div className='BigBox2'>
-      <div className='Topbox'>
-        {isMapFrameVisible && (
+      <div className='Topbox2'>
+        {isMapFrameVisible  &&(
           <>
             <MapFrame onMapClick={handleMapClick} />
        </>
+        )}
+          
+        {!isMapFrameVisible && jsonData?.PlotData && (
+          <>
+           
+            <h1>Area : {Math.floor(jsonData.PlotData.Area * 625) } Rai</h1>
+            <p>Latiude: {jsonData.PlotData.PlotCenter?.[0]}</p>
+            <p>Longitude : {jsonData.PlotData.PlotCenter?.[1]}</p>
+            <h1>Estimate </h1>
+            <h1>Carbon Credit Cap {Math.floor(jsonData.PlotData.Area * 625 * 0.6)} </h1>
+            <h1>Value in 1 year  {Math.floor(jsonData.PlotData.Area * 625 * 0.6) * 300} THB </h1>
+          </>
         )}
 
         {!isMapFrameVisible && (
           <>
             <button className="ClickBut" onClick={() => setIsMapFrameVisible(!isMapFrameVisible)}>Revert</button>
-          </>
+        </>
         )}
+
       </div>
-      <div className='BotBox'>
-        <div className='Subbox'></div>
-        <div className='Subbox'></div>
-      </div>
+    
     </div>
   );
 };
